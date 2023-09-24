2b3a5b6d178e637a05f9880243818b3161ffbcf9
mcszen1 <mcszen@gmail.com>
2023-09-24 09:31:13 -0300

    Update Feednews.py

--- Feednews.py
+++ Feednews.py
@@ -2,6 +2,7 @@ import feedparser
 import streamlit as st
 from urllib.parse import quote_plus
 
+
 def coletar_noticias(termo, limite=20):
     termo_codificado = quote_plus(termo)  # codificar o termo de pesquisa
     url = f"https://news.google.com/rss/search?q={termo_codificado}&hl=pt-BR&gl=BR&ceid=BR%3Apt-419"
@@ -17,8 +18,13 @@ def coletar_noticias(termo, limite=20):
     return noticias
 
 def main():
-    st.title("Coletor de Notícias")
-    
+    st.image('labcom_logo_preto.jpg')
+
+    st.title('AutoNEWS')
+
+    st.header('Coletor de notícias do LABCOM')
+    st.write('Digite um tópico e dê ENTER. Depois clique em BUSCAR NOTÍCIAS')
+        
     termo = st.text_input("Insira o termo de busca:", value="criptomoedas")
     if st.button("Buscar Notícias"):
         resultados = coletar_noticias(termo)
