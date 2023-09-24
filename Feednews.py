96db1cdbb5bcef144fd154fce9f762c9161230be
mcszen1 <mcszen@gmail.com>
2023-09-24 09:25:17 -0300

    Update Feednews.py

--- Feednews.py
+++ Feednews.py
@@ -1,8 +1,10 @@
 import feedparser
 import streamlit as st
+from urllib.parse import quote_plus
 
 def coletar_noticias(termo, limite=20):
-    url = f"https://news.google.com/rss/search?q={termo}&hl=pt-BR&gl=BR&ceid=BR%3Apt-419"
+    termo_codificado = quote_plus(termo)  # codificar o termo de pesquisa
+    url = f"https://news.google.com/rss/search?q={termo_codificado}&hl=pt-BR&gl=BR&ceid=BR%3Apt-419"
     feed = feedparser.parse(url)
     entradas = feed.entries[:limite]
     noticias = []
