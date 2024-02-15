c8cbc01cb2ffa80d587cf13fb8041bf4387cbaf7
mcszen1 <mcszen@gmail.com>
2024-02-15 18:13:22 -0300

    Update Release1.py

--- Release1.py
+++ Release1.py
@@ -26,7 +26,7 @@ def transcribe_audio():
  
     audio_file=file_path.read()
     
-    transcript = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+    transcript = client.audio.transcriptions.create(model="whisper-1", file=audio_file, response_format="text")
     return transcript['text']
 
 # Criação da interface do usuário no Streamlit
