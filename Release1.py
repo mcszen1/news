4384608ab311442324188cd0c9032adba45c0450
mcszen1 <mcszen@gmail.com>
2024-02-15 16:48:38 -0300

    Update Release1.py

--- Release1.py
+++ Release1.py
@@ -23,7 +23,7 @@ def generate_release_with_gpt(inputs):
     return response.choices[0].text
 
 def transcribe_audio(file_path):
-    audio_file = open(file_path,"rb").get_value()
+    audio_file = open(file_path,"rb").getvalue()
     transcript = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
     return transcript['text']
 
