ac0b115116eec9b88608b4e70d62ec86434738b8
mcszen1 <mcszen@gmail.com>
2024-02-18 08:51:48 -0300

    Update W2.py

--- W2.py
+++ W2.py
@@ -1,9 +1,12 @@
 import base64
 import streamlit as st
 from openai import OpenAI
+import os
 
 # Função para salvar o arquivo de áudio carregado localmente
-def save_uploaded_audio(uploaded_file_content, filename):
+def save_uploaded_audio(uploaded_file_content, uploaded_file_type):
+    extension = uploaded_file_type.split('/')[1]  # Obter a extensão do tipo de arquivo
+    filename = f"temp_audio.{extension}"
     with open(filename, "wb") as f:
         f.write(uploaded_file_content)
     return filename
@@ -21,9 +24,11 @@ def analyze_audio_with_whisper(audio_path):
 # Interface do usuário para upload de arquivo de áudio
 uploaded_file = st.file_uploader("Escolha um arquivo de áudio...", type=["mp3", "wav", "ogg", "flac"])
 if uploaded_file:
-    # Salvar o arquivo de áudio carregado localmente
-    audio_filename = save_uploaded_audio(uploaded_file.read(), "temp_audio.mp3")
-
+    if uploaded_file:
+    # Obter o tipo de mídia do arquivo carregado
+    audio_mimetype = uploaded_file.type
+    audio_filename = save_uploaded_audio(uploaded_file.read(), audio_mimetype)
+   
     # Exibir o áudio no Streamlit para que o usuário possa ouvir
     st.audio(audio_filename, format='audio/mp3', start_time=0)
 
